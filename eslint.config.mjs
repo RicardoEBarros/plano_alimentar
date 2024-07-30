e34832dc5631ee75cbf780c1de85c6c5e258cb03
RicardoEBarros <169519@upf.br>
2024-07-30 19:39:08 -0300

    chore: adicionar configurações finais ao eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import stylisticJs from '@stylistic/eslint-plugin-js'
 
 export default [
   {languageOptions: { globals: globals.browser }},
@@ -8,9 +9,21 @@ export default [
   ...tseslint.configs.recommended,
   {
     files: ["src/**/*.ts"],
-    ignore: ['node_modules'],
+    ignores: ['node_modules'],
+    plugins: {
+      "@stylistic/js": stylisticJs 
+    },
     rules: {
-      semi: ["error", "never"]
+      "semi": ["error", "never"],
+      "quotes": ["error", "double"],
+      "eqeqeq": "error",
+      "curly": "error",
+      "eol-last": ["error", "always"],
+      "no-eval": "error",
+      "no-unused-vars": "error",
+      "@stylistic/js/indent": ["error", 2],
+      "@stylistic/js/array-bracket-spacing": ["error", "always"],
+      "@stylistic/js/comma-spacing": ["error", { before: false, after: true }]
     }
   },
 ];

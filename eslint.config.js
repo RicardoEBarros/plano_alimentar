afa46cb917ab77fca59b16494e1c698aa449b788
RicardoEBarros <169519@upf.br>
2024-12-22 16:32:52 -0300

    chore: adicionar eslint

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+const globals = require("globals")
+const pluginJs = require("@eslint/js")
+const tseslint = require("typescript-eslint")
+const stylisticJs = require("@stylistic/eslint-plugin")
+
+module.exports = [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  { 
+    files: ["src/**/*.ts"],
+    ignores: ["node_modules", "dist", "coverage"],
+    plugins: {
+      '@stylistic/js': stylisticJs
+    },
+    rules: {
+      "semi": ["error", "never"],
+      "eqeqeq": "error",
+      "no-eval": "error",
+      "curly": ["warn", "all"],
+      "no-var": "warn",
+      "indent": ["error", 2],
+      "prefer-const": "warn",
+      "quotes": ["error", "single"],
+      "arrow-spacing": ["error", { before: true, after: true }],
+      "@typescript-eslint/strict-boolean-expressions": "off",
+      "@stylistic/js/function-call-spacing": ["error", "never"],
+      "@stylistic/js/array-bracket-spacing": ["error", "always"],
+      "@stylistic/js/eol-last": ["error", "always"],
+      "@stylistic/js/function-paren-newline": ["error", { minItems: 5 }],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off"
+    },
+    languageOptions: { 
+      globals: globals.browser 
+    }
+  }
+];

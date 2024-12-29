a744616673a80811f05ead945e88e8110b0dbdf4
RicardoEBarros <169519@upf.br>
2024-12-29 15:11:02 -0300

    feat: garantir RegistroController retorne um erro se nome não for fornecido

--- src/presentation/controllers/registro.ts
+++ src/presentation/controllers/registro.ts
@@ -2,7 +2,8 @@ export class RegistroController {
 
   async manipular(httpRequest: any): Promise<any> {
     return Promise.resolve({
-      statusCode: 400
+      statusCode: 400,
+      body: new Error('Parâmetro ausente: nome')
     })
   }
 

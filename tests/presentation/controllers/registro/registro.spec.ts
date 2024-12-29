40638784b3aa48230427f53b677fb03532828b00
RicardoEBarros <169519@upf.br>
2024-12-29 15:18:48 -0300

    test: garantir RegistroController retorne 400 se email não for fornecido

--- tests/presentation/controllers/registro/registro.spec.ts
+++ tests/presentation/controllers/registro/registro.spec.ts
@@ -22,4 +22,23 @@ describe('Registro Controller', () => {
 
   })
 
+  test('Deve retornar 400 se email não for informado', async () => {
+
+    const sut = new RegistroController()
+    const httpRequest = {
+      body: {
+        nome: 'nome_valido',
+        sexo: 'masculino',
+        idade: 20,
+        altura: 1.80,
+        peso: 80,
+        objetivo_final: 'definição'
+      }
+    }
+    const httpResponse = await sut.manipular(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Parâmetro ausente: email'))
+
+  })
+
 })

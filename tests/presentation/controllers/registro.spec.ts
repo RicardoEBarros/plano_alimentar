052fdbe5450fdd1802259b40b0bed22c23838d4d
RicardoEBarros <169519@upf.br>
2024-12-31 14:21:41 -0300

    test: garantir que ControleRegistro retorne 400 se objetivo_final não for informada

--- tests/presentation/controllers/registro.spec.ts
+++ tests/presentation/controllers/registro.spec.ts
@@ -137,4 +137,23 @@ describe('Controle Registro Suíte', () => {
 
   })
 
+  test('Deve retornar 400 se objetivo_final não for informado', async () => {
+
+    const sut = new ControleRegistro()
+    const httpRequest = {
+      body: {
+        nome: 'nome_valido',
+        email: 'email_valido',
+        sexo: 'masculino',
+        altura: 1.80,
+        peso: 80,
+        idade: 20
+      }
+    }
+    const httpResponse = await sut.manipular(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Parâmetro ausente: objetivo_final'))
+
+  })
+
 })

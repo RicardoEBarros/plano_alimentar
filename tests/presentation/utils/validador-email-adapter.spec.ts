9e8eb9848d70f45b036ca83e69db32c478a16a60
RicardoEBarros <169519@upf.br>
2025-01-02 11:00:27 -0300

    test: garantir que ValidadorEmailAdapter retorne true se validador retornar true

--- tests/presentation/utils/validador-email-adapter.spec.ts
+++ tests/presentation/utils/validador-email-adapter.spec.ts
@@ -1,12 +1,26 @@
 import { describe, test, expect } from '@jest/globals'
 import { ValidadorEmailAdapter } from '@utils/validador-email-adapter'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail(): boolean {
+    return true
+  }
+}))
 
 describe('ValidadorEmailAdapter Suíte', () => {
 
   test('Deve retornar false is validador retornar false', () => {
     const sut = new ValidadorEmailAdapter()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const emailValido = sut.emailValido('email_invalido@mail.com')
     expect(emailValido).toBe(false)
+  })
+
+  test('Deve retornar true is validador retornar true', () => {
+    const sut = new ValidadorEmailAdapter()
+    const emailValido = sut.emailValido('email_valido@mail.com')
+    expect(emailValido).toBe(true)
   })
 
 })

8c60f47f61fa8a23fb078b618decd7bded05f274
RicardoEBarros <169519@upf.br>
2025-01-05 11:20:32 -0300

    test: garantir que BcryptAdapter retorne uma hash se tudo der certo

--- tests/infra/criptografia/bcrypt-adapter.spec.ts
+++ tests/infra/criptografia/bcrypt-adapter.spec.ts
@@ -2,6 +2,12 @@ import { describe, test, jest, expect } from '@jest/globals'
 import { BcryptAdapter } from '@infra/criptografia/bcrypt-adapter'
 import bcrypt from 'bcrypt'
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return Promise.resolve('hash')
+  }
+}))
+
 describe('BcryptAdapter Suíte', () => {
 
   test('Deve chamar bcrypt com os valores corretos ', async () => {
@@ -11,6 +17,15 @@ describe('BcryptAdapter Suíte', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encriptar('valor_sem_encriptação')
     expect(hashSpy).toHaveBeenNthCalledWith(1, 'valor_sem_encriptação', salt)
+
+  })
+
+  test('Deve retornar uma hash se tudo der certo', async () => {
+
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const encriptado = await sut.encriptar('valor_sem_encriptação')
+    expect(encriptado).toBe('hash')
 
   })
 

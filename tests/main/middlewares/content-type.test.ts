b7df8b22067504800e44096d2670bb45ae0c65b9
RicardoEBarros <169519@upf.br>
2025-01-12 15:52:05 -0300

    test: garantir que requisições retornem o content-type como json por padrão

---
+++ tests/main/middlewares/content-type.test.ts
@@ -0,0 +1,32 @@
+import { describe, test } from '@jest/globals'
+import app from '@main/config/app'
+import request from 'supertest'
+
+describe('Content Type Middleware', () => {
+
+  test('Deve retornar o content-type como json', async () => {
+
+    app.get('/test_content_type', (req, res) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+
+  })
+
+  test('Deve retornar um xml quando forçado', async () => {
+
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send()
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+
+  })
+
+})

f785207520902f6704405e7b099b4908486be379
RicardoEBarros <169519@upf.br>
2025-01-12 15:33:14 -0300

    test: garantir que cors está habilitado

---
+++ tests/main/middlewares/cors.test.ts
@@ -0,0 +1,21 @@
+import { describe, test } from '@jest/globals'
+import app from '@main/config/app'
+import request from 'supertest'
+
+describe('CORS Middleware', () => {
+
+  test('Should enable CORS', async () => {
+
+    app.get('/test_cors', (req, res) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/test_cors')
+      .expect('access-controll-allow-origin', '*')
+      .expect('access-controll-allow-methods', '*')
+      .expect('access-controll-allow-headers', '*')
+
+  })
+
+})

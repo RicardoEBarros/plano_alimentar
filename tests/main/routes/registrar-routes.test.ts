47bf773d16152794a1b6ca2f78160c77c02831cd
RicardoEBarros <169519@upf.br>
2025-01-16 10:40:51 -0300

    refactor: conectar ao mongodb antes dos testes de integração

--- tests/main/routes/registrar-routes.test.ts
+++ tests/main/routes/registrar-routes.test.ts
@@ -1,9 +1,23 @@
-import { describe, test } from '@jest/globals'
+import { describe, test, beforeAll, beforeEach, afterAll } from '@jest/globals'
+import { RegistradorObjectMother } from '@/tests/mocks/object-mothers/presentation/controllers/registro/registrador-object-mother'
+import { MongoHelper } from '@/src/infra/db/mongodb/helpers/mongo-helper'
 import app from '@/src/main/config/app'
 import request from 'supertest'
-import { RegistradorObjectMother } from '@/tests/mocks/object-mothers/presentation/controllers/registro/registrador-object-mother'
 
 describe('Registrar Rotas Suíte', () => {
+
+  beforeAll(async () => {
+    await MongoHelper.conectar(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    const contaCollection = MongoHelper.getCollection('contas')
+    await contaCollection.deleteMany({})
+  })
+
+  afterAll(async () => {  
+    await MongoHelper.disconectar()
+  })
 
   test('Deve retorna uma conta se tudo der certo', async () => {
 

26e7f928fc0683b876bef1a992e238e90de2ec6a
RicardoEBarros <169519@upf.br>
2025-01-16 10:58:38 -0300

    feat: adicionar factory para registro de conta

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/plano-alimentar',
-  porta: process.env.PORT ? process.env.PORT : 5050
+  porta: process.env.PORT ? process.env.PORT : 5050,
+  saltDoEncriptador: 12
 }
---
+++ src/main/factories/registrar.ts
@@ -0,0 +1,14 @@
+import { RegistradorContaDb } from '@/src/data/usecases/registrador-conta/registrador-conta-db'
+import { BcryptAdapter } from '@/src/infra/criptografia/bcrypt-adapter'
+import { RegistroController } from '@/src/presentation/controllers/registrador-conta/registro-controller'
+import { ValidadorEmailAdapter } from '@/src/utils/validador-email-adapter'
+import { ContaMongoRepository } from '@/src/infra/db/mongodb/conta-repository/conta'
+import env from '../config/env'
+
+export const makeRegistroController = (): RegistroController => {
+  const validadorEmail = new ValidadorEmailAdapter()
+  const bcryptAdapter = new BcryptAdapter(env.saltDoEncriptador) 
+  const registradorContaRepository = new ContaMongoRepository()
+  const registradorConta = new RegistradorContaDb(bcryptAdapter, registradorContaRepository)
+  return new RegistroController(validadorEmail, registradorConta)
+}

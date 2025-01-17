a8d65e0fb819f8dcc5bb4ec94dde91f46a2b32ec
RicardoEBarros <169519@upf.br>
2025-01-17 13:54:58 -0300

    test: garantir que LogControllerDecorator retorne o mesmo resultado do controller

--- tests/main/decorators/log.spec.ts
+++ tests/main/decorators/log.spec.ts
@@ -1,6 +1,7 @@
 import { describe, test, expect, jest } from '@jest/globals'
 import { RegistradorObjectMother } from '@/tests/mocks/object-mothers/presentation/controllers/registro/registrador-object-mother'
 import { makeLogControllerSut } from '@/tests/mocks/factories/main/decorators/log-factory'
+import { RegistradorContaRepositoryObjectMother } from '@/tests/mocks/object-mothers/data/usecases/registrador-conta/registrador-conta-repository-object-mother'
 
 describe('LogControllerDecorator Suíte', () => {
 
@@ -13,6 +14,17 @@ describe('LogControllerDecorator Suíte', () => {
     await sut.manipular(httpRequest)
 
     expect(manipularSpy).toHaveBeenCalledWith(httpRequest)
+
+  })
+
+  test('Deve retorne o mesmo resultado do controller', async () => {
+
+    const { sut } = makeLogControllerSut()
+    const contaFake = RegistradorObjectMother.idAusente()
+    const httpRequest = { body: contaFake  }
+    const httpResponse = await sut.manipular(httpRequest)
+
+    expect(httpResponse).toEqual({ statusCode: 200, body: RegistradorContaRepositoryObjectMother.confirmarPasswordAusente() })
 
   })
 

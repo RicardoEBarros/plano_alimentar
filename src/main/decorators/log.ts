82e192813911aa9975fe7dda3db7d8e42bbca535
RicardoEBarros <169519@upf.br>
2025-01-17 15:10:26 -0300

    feat: garantir que LogControllerDecorator chame LogErrorRepository se controller retornar um internal server error

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,11 +1,15 @@
+import { LogErrorRepository } from '@/src/data/protocols/log-error-repository'
 import { Controller, HttpRequest, HttpResponse } from '@/src/presentation/protocols'
 
 export class LogControllerDecorator implements Controller {
 
-  constructor(private readonly controller: Controller) {}
+  constructor(private readonly controller: Controller, private readonly logErrorRepository: LogErrorRepository) {}
 
   async manipular(httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.manipular(httpRequest)
+    if (httpResponse.statusCode === 500) {
+      await this.logErrorRepository.log(httpResponse.body?.stack)
+    }
     return httpResponse
   }
 

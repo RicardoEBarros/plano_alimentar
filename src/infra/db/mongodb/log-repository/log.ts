4d42aed42fc5044a57af632c7f3902035a52a91c
RicardoEBarros <169519@upf.br>
2025-01-20 10:51:01 -0300

    feat: garantir que LogMongoRepository vai criar um log de erro se tudo der certo

---
+++ src/infra/db/mongodb/log-repository/log.ts
@@ -0,0 +1,14 @@
+import { LogErrorRepository } from '@/src/data/protocols/log-error-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class LogMongoRepository implements LogErrorRepository {
+  
+  async logError(pilhaErros: string): Promise<void> {
+    const logErrosCollection = await MongoHelper.getCollection('log-erros')
+    await logErrosCollection.insertOne({
+      pilhaErros,
+      date: new Date()
+    })
+  }
+
+}

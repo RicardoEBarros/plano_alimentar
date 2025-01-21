b329a1cf2a0cb0d1ee357f2609c5f9886189a385
RicardoEBarros <169519@upf.br>
2025-01-21 10:53:39 -0300

    feat: garantir que LoginController chame ValidadorEmail com o email correto

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,16 +1,21 @@
-import { ParametroAusenteError } from '../../errors'
+import { ParametroAusenteError, ParametroInvalidoError } from '../../errors'
 import { badRequest, ok } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { ValidadorEmail } from '../registrador-conta/registro-protocols'
 
 export class LoginController implements Controller {
 
+  constructor(private readonly validadorEmail: ValidadorEmail) {}
+
   async manipular(httpRequest: HttpRequest): Promise<HttpResponse> {
-    
+
     if (!httpRequest.body.email) {
       return Promise.resolve(badRequest(new ParametroAusenteError('email')))
     } else if (!httpRequest.body.password) {
       return Promise.resolve(badRequest(new ParametroAusenteError('password')))  
     }
+
+    this.validadorEmail.emailValido(httpRequest.body.email)
 
     return Promise.resolve(ok({}))
 

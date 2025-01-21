b17966ac2e7f5358342ae95d85fae3283fb384ce
RicardoEBarros <169519@upf.br>
2025-01-21 09:45:42 -0300

    feat: garantir que LoginController retorne 400 se password não for fornecido

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,11 +1,19 @@
 import { ParametroAusenteError } from '../../errors'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, ok } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 
 export class LoginController implements Controller {
 
   async manipular(httpRequest: HttpRequest): Promise<HttpResponse> {
-    return Promise.resolve(badRequest(new ParametroAusenteError('email')))
+    
+    if (!httpRequest.body.email) {
+      return Promise.resolve(badRequest(new ParametroAusenteError('email')))
+    } else if (!httpRequest.body.password) {
+      return Promise.resolve(badRequest(new ParametroAusenteError('password')))  
+    }
+
+    return Promise.resolve(ok({}))
+
   }
 
 }

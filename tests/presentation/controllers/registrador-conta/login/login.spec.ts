5e4169197fcf83d199dce56313ef779fd8562dbb
RicardoEBarros <169519@upf.br>
2025-01-21 09:42:00 -0300

    test: garantir que LoginController retorne 400 se email não for fornecido

---
+++ tests/presentation/controllers/registrador-conta/login/login.spec.ts
@@ -0,0 +1,21 @@
+import { LoginController } from '@/src/presentation/controllers/login/login'
+import { ParametroAusenteError } from '@/src/presentation/errors'
+import { badRequest } from '@/src/presentation/helpers/http-helper'
+import { describe, test, expect } from '@jest/globals'
+
+describe('Login Controller Suíte', () => {
+
+  test('Deve retornar 400 se um email não for fornecido', async () => {
+
+    const sut = new LoginController()
+    const httpRequest = {
+      body: {
+        password: 'password_valido'
+      }
+    }
+    const httpResponse = await sut.manipular(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new ParametroAusenteError('email')))
+
+  })
+
+})

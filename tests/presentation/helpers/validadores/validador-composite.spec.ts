08bfea7cfdaa59baad0c79fdb5c34eaabbdd0774
RicardoEBarros <169519@upf.br>
2025-01-27 13:40:46 -0300

    test: garantir que ValidadorComposite retorne o correto ao falhar

---
+++ tests/presentation/helpers/validadores/validador-composite.spec.ts
@@ -0,0 +1,19 @@
+import { ValidadorComposite } from '@/src/presentation/helpers/validadores'
+import { makeValidacaoSexo } from '@/tests/mocks/factories/presentation/helpers/validadores/validacao-sexo-factory'
+import { RegistradorObjectMother } from '@/tests/mocks/object-mothers/presentation/controllers/registro/registrador-object-mother'
+import { describe, test, expect, jest } from '@jest/globals'
+
+describe('Validador Composite Suíte', () => {
+
+  test('Deve retornar um erro se validação falhar', () => {
+
+    const validadorSexo = makeValidacaoSexo()
+    jest.spyOn(validadorSexo, 'validar').mockReturnValueOnce(new Error())
+    const validadorComposite = new ValidadorComposite([ validadorSexo ])
+    const erro = validadorComposite.validar(RegistradorObjectMother.idAusente())
+
+    expect(erro).toEqual(new Error())
+
+  })
+
+})

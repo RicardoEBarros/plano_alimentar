823f2ca3e916eb3c177ef4d79eca791cc3c0a1e7
RicardoEBarros <169519@upf.br>
2025-01-31 09:47:38 -0300

    feat: garantir que AutenticadorDb chame BuscarContaPorEmailRepository com o email correto

---
+++ src/data/protocols/buscar-conta-por-email-repository.ts
@@ -0,0 +1,5 @@
+import { ContaModel } from '../usecases/registrador-conta/registrador-conta-db-protocols'
+
+export interface BuscarContaPorEmailRepository {
+  buscar(email: string): Promise<ContaModel>
+}
---
+++ src/data/usecases/autenticador/autenticador-db.ts
@@ -0,0 +1,13 @@
+import { Autenticador, AutenticadorModel } from '@/src/domain/usecases/autenticador'
+import { BuscarContaPorEmailRepository } from '../../protocols/buscar-conta-por-email-repository'
+
+export class AutenticadorDb implements Autenticador {
+
+  constructor(private readonly buscarContaPorEmailRepository: BuscarContaPorEmailRepository) {}
+  
+  async autenticar(autenticacao: AutenticadorModel): Promise<string> {
+    await this.buscarContaPorEmailRepository.buscar(autenticacao.email)
+    return ''
+  }
+  
+}

d84270c75c271cef67985e25b6f4bb3d1f63a30e
RicardoEBarros <169519@upf.br>
2025-02-03 15:24:02 -0300

    feat: adicionar rota de login

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { adaptadorRota } from '../adapter/express/express-route-adapter'
 import { makeRegistroController } from '../factories/registro/registro-factory'
+import { makeLoginController } from '../factories/login/login-factory'
 
 export default (router: Router): void => {
   router.post('/registrar', adaptadorRota(makeRegistroController()))
+  router.post('/login', adaptadorRota(makeLoginController()))
 }

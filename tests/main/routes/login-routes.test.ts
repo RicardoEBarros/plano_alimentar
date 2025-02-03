e88906e60e6ae8d7dc8c29cc49f2f351d22e66b8
RicardoEBarros <169519@upf.br>
2025-02-03 15:24:21 -0300

    test: adicionar rota de login

--- tests/main/routes/login-routes.test.ts
+++ tests/main/routes/login-routes.test.ts
@@ -1,17 +1,24 @@
 import { describe, test, beforeAll, beforeEach, afterAll } from '@jest/globals'
 import { RegistradorObjectMother } from '@/tests/mocks/object-mothers/presentation/controllers/registro/registrador-object-mother'
 import { MongoHelper } from '@/src/infra/db/mongodb/helpers/mongo-helper'
+import { LoginObjectMother } from '@/tests/mocks/object-mothers/presentation/controllers/login/login-object-mother'
+import { Collection } from 'mongodb'
 import app from '@/src/main/config/app'
 import request from 'supertest'
+import { hash } from 'bcrypt'
+import { ContaModel } from '@/src/domain/models/conta'
+import env from '@/src/main/config/env'
 
 describe('Rotas Login Suíte', () => {
+
+  let contaCollection: Collection
 
   beforeAll(async () => {
     await MongoHelper.conectar(process.env.MONGO_URL)
   })
 
   beforeEach(async () => {
-    const contaCollection = await MongoHelper.getCollection('contas')
+    contaCollection = await MongoHelper.getCollection('contas')
     await contaCollection.deleteMany({})
   })
 
@@ -28,6 +35,24 @@ describe('Rotas Login Suíte', () => {
       await request(app)
         .post('/api/registrar')
         .send(contaFake)
+        .expect(200)
+  
+    })
+
+  })
+
+  describe('POST /login', () => {
+    
+    test('Deve retornar 200 ao logar usuário', async () => {
+
+      const loginFake = LoginObjectMother.valido()
+      const registroFake = RegistradorObjectMother.idAusente() as ContaModel
+      registroFake.password = await hash(registroFake.password, env.salt)
+      await contaCollection.insertOne(registroFake)
+
+      await request(app)
+        .post('/api/login')
+        .send(loginFake)
         .expect(200)
   
     })

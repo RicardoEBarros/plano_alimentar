66053885d3c453feacf9b305343ff27369a839e0
RicardoEBarros <169519@upf.br>
2025-02-10 15:31:47 -0300

    chore: habilitar debug com vscode

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const { pathsToModuleNameMapper } = require("ts-jest");
 
 module.exports = {
   injectGlobals: false,
-  roots: ['<rootDir>/tests', '<rootDir>/src'],
+  roots: ['<rootDir>/tests'],
   preset: '@shelf/jest-mongodb',
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',

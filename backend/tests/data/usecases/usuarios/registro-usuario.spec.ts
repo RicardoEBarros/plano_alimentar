d82eb66ec64c215b5fd9dd86e6e854697dcde091
RicardoEBarros <169519@upf.br>
2025-03-30 14:01:06 -0300

    test: garantir que RegistroUsuario lance uma exceção se Encriptador lançar um erro

--- backend/tests/data/usecases/usuarios/registro-usuario.spec.ts
+++ backend/tests/data/usecases/usuarios/registro-usuario.spec.ts
@@ -13,4 +13,20 @@ describe("Registro Usuário Suíte", () => {
 
   })
 
+  test("Deve lançar uma exceção se Encriptador lançar um erro", async () => {
+
+    const { sut, parametrosFake, encriptadorStub } = makeRegistroUsuarioFactory()
+    jest.spyOn(encriptadorStub, "encriptar").mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.registrar(parametrosFake.usuario)
+
+    await expect(promise).rejects.toThrow(new Error())
+
+  })
+
+  test.todo("Deve chamar RegistradorUsuario com os parâmetros corretos")
+  test.todo("Deve lançar uma exceção se RegistradorUsuario lançar um erro")
+  test.todo("Deve chamar GeradorToken com os parâmetros corretos")
+  test.todo("Deve lançar uma exceção se GeradorToken lançar um erro")
+  test.todo("Deve retornar o token de acesso se tudo der certo")
+
 })

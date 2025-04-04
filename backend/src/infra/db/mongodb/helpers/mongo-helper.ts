cf544d71c49b7f6ef1e38e311b7bacb369919262
RicardoEBarros <169519@upf.br>
2025-04-04 19:28:31 -0300

    feat: garantir que MongoHelper chame MongoCliente.connect com o parâmetro correto

---
+++ backend/src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,13 @@
+import { MongoClient } from "mongodb"
+
+export const MongoHelper = {
+
+  uri: null as string,
+  client: null as MongoClient,
+
+  async conectar(uri: string): Promise<void> {
+    this.uri = uri
+    this.client = await MongoClient.connect(uri)
+  }
+
+}

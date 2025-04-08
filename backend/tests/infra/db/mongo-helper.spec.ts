cc4bdb36fb35b9b30e2d49b2e9b2f8e94235d434
RicardoEBarros <169519@upf.br>
2025-04-08 19:48:28 -0300

    test: garantir que MongoHelper.disconectar chame o método close do mongodb

--- backend/tests/infra/db/mongo-helper.spec.ts
+++ backend/tests/infra/db/mongo-helper.spec.ts
@@ -46,4 +46,22 @@ describe("Mongo Helper Suíte", () => {
 
   })
 
+  describe("Disconectar Suíte", () => {
+
+    test("Deve desconectar do mongodb", async () => {
+
+      const mockClient = { close: jest.fn() } as unknown as MongoClient
+      const uri = faker.lorem.word()
+      jest.spyOn(MongoClient, "connect").mockResolvedValueOnce(mockClient)
+      await MongoHelper.conectar(uri)
+      await MongoHelper.disconectar()   
+
+      expect(MongoHelper.client.close).toHaveBeenCalledTimes(1)
+
+    })
+
+    test.todo("Deve lançar uma exceção se MongoClient.close lançar um erro")
+
+  })
+
 })

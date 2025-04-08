d037d3445ad6f3a11bb1d4c660a0381116eb350e
RicardoEBarros <169519@upf.br>
2025-04-08 19:47:50 -0300

    feat: garantir que MongoHelper.disconectar chame o método close do mongodb

--- backend/src/infra/db/mongodb/helpers/mongo-helper.ts
+++ backend/src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -8,6 +8,10 @@ export const MongoHelper = {
   async conectar(uri: string): Promise<void> {
     this.uri = uri
     this.client = await MongoClient.connect(uri)
+  },
+
+  async disconectar(): Promise<void> {
+    await this.client.close()
   }
 
 }

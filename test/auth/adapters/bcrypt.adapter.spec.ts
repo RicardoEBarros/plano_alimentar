3150215fb0b10838fbee606fd885d5232c9ce6ee
RicardoEBarros <169519@upf.br>
2025-07-25 10:14:49 -0300

    test: garantir que BcryptAdapter chame bcrypt com o salt correto

--- test/auth/adapters/bcrypt.adapter.spec.ts
+++ test/auth/adapters/bcrypt.adapter.spec.ts
@@ -1,11 +1,17 @@
 import { DEFAULT_NUMBER_OF_SALTS_FOR_THE_HASH } from '@/src/auth/constants/hash.constant'
 import { makeBcryptAdapterFactory } from './mocks/bcrypt.adapter.factory'
+import { BcryptAdapter } from '@/src/auth/adapters/bcrypt.adapter'
+import { faker } from '@faker-js/faker/.'
 import * as bcrypt from 'bcrypt'
 
 jest.mock('bcrypt')
 const bcryptMocked = bcrypt as jest.Mocked<typeof bcrypt>
 
 describe('Bcrypt Adapter Suite', () => {
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
 
   test('Should instantiate a new BcryptAdapter successfully', async () => {
     const { sut } = await makeBcryptAdapterFactory()
@@ -15,10 +21,22 @@ describe('Bcrypt Adapter Suite', () => {
   test('Should calls bcrypt with correct values', async () => {
 
     const { sut, fakeParameters } = await makeBcryptAdapterFactory()
-    sut.hash(fakeParameters.passwordWithoutHash)
+    await sut.hash(fakeParameters.passwordWithoutHash)
 
     expect(bcryptMocked.hash).toHaveBeenCalledOnce()
     expect(bcryptMocked.hash).toHaveBeenCalledWith(fakeParameters.passwordWithoutHash, DEFAULT_NUMBER_OF_SALTS_FOR_THE_HASH)
+
+  })
+
+  test('Should calls bcrypt with salt correct when passed through the constructor', async () => {
+
+    const salt = faker.number.int()
+    const { fakeParameters } = await makeBcryptAdapterFactory()
+    const sut = new BcryptAdapter(salt)
+    await sut.hash(fakeParameters.passwordWithoutHash)
+
+    expect(bcryptMocked.hash).toHaveBeenCalledOnce()
+    expect(bcryptMocked.hash).toHaveBeenCalledWith(fakeParameters.passwordWithoutHash, salt)
 
   })
 

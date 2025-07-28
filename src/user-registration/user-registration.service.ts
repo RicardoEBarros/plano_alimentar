3fcf533deca04d0faa77458fa0ddda2eea2b6e2e
RicardoEBarros <169519@upf.br>
2025-07-28 18:23:59 -0300

    feat: garantir que UserRegistrationService chame idNormalizer com os parâmetros corretos

--- src/user-registration/user-registration.service.ts
+++ src/user-registration/user-registration.service.ts
@@ -4,13 +4,15 @@ import { UserEntity } from './entities/user.entity'
 import { InjectModel } from '@nestjs/mongoose'
 import { User, UserDocument } from './schema/user.shema'
 import { Model } from 'mongoose'
+import { ReplaceIdentifier } from '../database/contracts/replace-identifier.contract'
 
 @Injectable()
 export class UserRegistrationService implements FindUserByEmail {
   
   constructor(
     @InjectModel(User.name)
-    private readonly userModel: Model<User>
+    private readonly userModel: Model<User>,
+    private readonly idNormalizer: ReplaceIdentifier
   ) {}
 
   async findByEmail(email: string): Promise<UserEntity | null> {
@@ -21,6 +23,8 @@ export class UserRegistrationService implements FindUserByEmail {
       return null
     }
     
+    this.idNormalizer.replaceId(user)
+
     return {} as UserEntity
 
   }
